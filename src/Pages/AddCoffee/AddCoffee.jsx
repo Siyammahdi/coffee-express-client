6812338218f892551fe71dd2b1070be15b2749f9
Siyammahdi <siyammahdi1@gmail.com>
2023-10-20 23:14:52 +0600

    coffee data get done

--- src/Pages/AddCoffee/AddCoffee.jsx
+++ src/Pages/AddCoffee/AddCoffee.jsx
@@ -9,6 +9,24 @@ const AddCoffee = () => {
         navigate(-1)
     };
 
+
+    const handleAddCoffee = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photoURL = form.photoURL.value;
+
+        const newCoffee = {name, chef, supplier, taste, category, details, photoURL};
+        console.log(newCoffee);
+
+
+    }
+
     return (
         <div className="my-32 text-center">
             <div className="flex justify-start mt-10 font-rancho ml-36 ">
@@ -17,42 +35,44 @@ const AddCoffee = () => {
             <div className="bg-card-bg mx-36 py-20 my-10">
                 <h1 className="text-3xl text-primary-color font-semibold">Add New Coffee</h1>
                 <p className="font-sans w-3/4 m-auto my-10">It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
-                <div className="mx-28 font-sans font-semibold">
-                    <div className="grid grid-cols-2 grid-rows-3 gap-5 ">
-                        <div>
-                            <h3 className="text-left my-2 text-lg">Name</h3>
-                            <input type="text" placeholder="Enter coffee name" className="input w-full" />
+                <form onSubmit={handleAddCoffee}>
+                    <div className="mx-28 font-sans font-semibold">
+                        <div className="grid grid-cols-2 grid-rows-3 gap-5 ">
+                            <label>
+                                <h3 className="text-left my-2 text-lg">Name</h3>
+                                <input type="text" name="name" placeholder="Enter coffee name" className="input w-full" />
+                            </label>
+                            <label>
+                                <h3 className="text-left my-2 text-lg">Chef</h3>
+                                <input type="text" name="chef" placeholder="Enter coffee chef" className="input w-full" />
+                            </label>
+                            <label>
+                                <h3 className="text-left my-2 text-lg">Supplier</h3>
+                                <input type="text" name="supplier" placeholder="Enter coffee supplier" className="input w-full" />
+                            </label>
+                            <label>
+                                <h3 className="text-left my-2 text-lg">Taste</h3>
+                                <input type="text" name="taste" placeholder="Enter coffee taste" className="input w-full" />
+                            </label>
+                            <label>
+                                <h3 className="text-left my-2 text-lg">Category</h3>
+                                <input type="text" name="category" placeholder="Enter coffee category" className="input w-full" />
+                            </label>
+                            <label>
+                                <h3 className="text-left my-2 text-lg">Details</h3>
+                                <input type="text" name="details" placeholder="Enter coffee details" className="input w-full" />
+                            </label>
                         </div>
-                        <div>
-                            <h3 className="text-left my-2 text-lg">Chef</h3>
-                            <input type="text" placeholder="Enter coffee chef" className="input w-full" />
-                        </div>
-                        <div>
-                            <h3 className="text-left my-2 text-lg">Supplier</h3>
-                            <input type="text" placeholder="Enter coffee supplier" className="input w-full" />
-                        </div>
-                        <div>
-                            <h3 className="text-left my-2 text-lg">Taste</h3>
-                            <input type="text" placeholder="Enter coffee taste" className="input w-full" />
-                        </div>
-                        <div>
-                            <h3 className="text-left my-2 text-lg">Category</h3>
-                            <input type="text" placeholder="Enter coffee category" className="input w-full" />
-                        </div>
-                        <div>
-                            <h3 className="text-left my-2 text-lg">Details</h3>
-                            <input type="text" placeholder="Enter coffee details" className="input w-full" />
-                        </div>
+                        <label>
+                            <h3 className="text-left mt-5 mb-2 text-lg">Photo</h3>
+                            <input type="text" name="photoURL" placeholder="Enter photo URL" className="input w-full" />
+                        </label>
+                        <input type="submit" className="btn btn-block my-5 bg-block-btn-bg text-primary-color border-2 border-primary-color font-rancho hover:bg-primary-color hover:text-block-btn-bg hover:border-block-btn-bg" value="Add Coffee" />
                     </div>
-                    <div>
-                        <h3 className="text-left mt-5 mb-2 text-lg">Photo</h3>
-                        <input type="text" placeholder="Enter photo URL" className="input w-full" />
-                    </div>
-                    <button className="btn btn-block my-5 bg-block-btn-bg text-primary-color border-2 border-primary-color font-rancho">Add Coffee</button>
-                </div>
+                </form>
             </div>
         </div>
     );
-};
+}; 
 
 export default AddCoffee;
--- src/Pages/UpdateCoffee/UpdateCoffee.jsx
+++ src/Pages/UpdateCoffee/UpdateCoffee.jsx
@@ -16,39 +16,39 @@ const UpdateCoffee = () => {
         <div className="bg-card-bg mx-36 py-20">
             <h1 className="text-3xl drop-shadow-xl font-semibold">Update Existing Coffee Details</h1>
             <p className="font-sans w-3/4 m-auto my-10">It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
-            <div className="mx-28 font-sans font-semibold">
+            <form className="mx-28 font-sans font-semibold">
                 <div className="grid grid-cols-2 grid-rows-3 gap-5 ">
                     <div>
                         <h3 className="text-left my-2 text-lg">Name</h3>
-                        <input type="text" placeholder="Enter coffee name" className="input w-full" />
+                        <input type="text" name="name" placeholder="Enter coffee name" className="input w-full" />
                     </div>
                     <div>
                         <h3 className="text-left my-2 text-lg">Chef</h3>
-                        <input type="text" placeholder="Enter coffee chef" className="input w-full" />
+                        <input type="text" name="chef" placeholder="Enter coffee chef" className="input w-full" />
                     </div>
                     <div>
                         <h3 className="text-left my-2 text-lg">Supplier</h3>
-                        <input type="text" placeholder="Enter coffee supplier" className="input w-full" />
+                        <input type="text" name="supplier" placeholder="Enter coffee supplier" className="input w-full" />
                     </div>
                     <div>
                         <h3 className="text-left my-2 text-lg">Taste</h3>
-                        <input type="text" placeholder="Enter coffee taste" className="input w-full" />
+                        <input type="text" name="taste" placeholder="Enter coffee taste" className="input w-full" />
                     </div>
                     <div>
                         <h3 className="text-left my-2 text-lg">Category</h3>
-                        <input type="text" placeholder="Enter coffee category" className="input w-full" />
+                        <input type="text" name="category" placeholder="Enter coffee category" className="input w-full" />
                     </div>
                     <div>
                         <h3 className="text-left my-2 text-lg">Details</h3>
-                        <input type="text" placeholder="Enter coffee details" className="input w-full" />
+                        <input type="text" name="details" placeholder="Enter coffee details" className="input w-full" />
                     </div>
                 </div>
                 <div>
                     <h3 className="text-left mt-5 mb-2 text-lg">Photo</h3>
-                    <input type="text" placeholder="Enter photo URL" className="input w-full" />
+                    <input type="text" name="photoURL" placeholder="Enter photo URL" className="input w-full" />
                 </div>
-                <button className="btn btn-block my-5 bg-block-btn-bg text-primary-color border-2 border-primary-color font-rancho">Update Coffee Details</button>
-            </div>
+                <input type="button"  className="btn btn-block my-5 bg-block-btn-bg text-primary-color border-2 border-primary-color font-rancho">Update Coffee Details</input>
+            </form>
         </div>
     </div>
     );
